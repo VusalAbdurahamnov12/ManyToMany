26fb72a7035fd775bdd99007ca6d421a9c3c3cd1
test <sh13ld001@gmail.com>
2022-05-24 08:11:10 +0400

    first commit

---
+++ WebApplication1/WebApplication1/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+                
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Category
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Logo { get; set; }
+    }
+}
