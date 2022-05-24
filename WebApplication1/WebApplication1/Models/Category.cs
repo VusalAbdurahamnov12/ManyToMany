e9ae91d858c1fc44293f8a27e9a5f4448fac316e
test <sh13ld001@gmail.com>
2022-05-24 08:36:59 +0400

    migration

--- WebApplication1/WebApplication1/Models/Category.cs
+++ WebApplication1/WebApplication1/Models/Category.cs
@@ -2,7 +2,7 @@
 {
     public class Category
     {
-        int Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         
         public string Logo { get; set; }
